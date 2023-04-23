331f5ec416386de682fb7560597ce2ea646546f2
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-04-23 18:36:20 -0400

    chart.js integration

---
+++ script.js
@@ -0,0 +1,39 @@
+function initChart(chart) {
+  const labels = ["January", "February", "March", "April", "May", "June"];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+
+  const config = {
+    type: "line",
+    data: data,
+    options: {},
+  };
+
+  return new Chart(
+    chart, 
+    config
+  );
+}
+
+async function mainEvent() {
+  const chartTarget = document.querySelector("#myChart");
+
+  const results = await fetch ('https://beta.umd.io/');
+//   convert results to JSON
+  const arrayFromJson = await results.json();
+
+  initChart(chartTarget);
+}
+
+// the async keyword means we can make API requests
+document.addEventListener("DOMContentLoaded", async () => mainEvent());
