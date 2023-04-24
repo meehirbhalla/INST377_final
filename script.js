c399de1e72aab8c30921b5fa206ad90e1e479ada
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-04-23 22:27:28 -0400

    chart.js implementation

--- script.js
+++ script.js
@@ -14,4 +14,26 @@ async function processing_request() {
   const openSections = courses.filter((item) => item.open_seats > 0);
   const sections = openSections.map((item) => "Section ${item.section_id}");
   const openSeats = openSections.map((item) => item.open_seats);
+
+  const chartTarget = document.querySelector("#myChart");
+
+  const labels = ["January", "February", "March", "April", "May", "June"];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+
+  const config = {
+    type: "line",
+    data: data,
+    options: {},
+  };
 }
