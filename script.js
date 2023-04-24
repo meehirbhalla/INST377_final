3205ec8bbcc8bf3897e02f5d1a723d007b47f846
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-04-23 22:19:13 -0400

    modify to only get data on INST377

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // An asynchronous data request to your API
 async function getData() {
-  const url = "https://api.umd.io/v0/courses/list";
+  const url = "https://api.umd.io/v0/courses/INST377/sections";
   const data = await fetch(url);
   const json_data = await data.json();
+  return json_data;
 }
 
 // A processing request that uses array methods (.map, .filter, .find, .reduce) to change
@@ -18,5 +19,12 @@ async function processing_request() {
     const sectionJson = await sectionData.json();
 
     const filterSections = sectionJson.filter(item => item.open_seats > 0);
+
+    openSections.push({
+      course_id: course.course_id,
+      open_sections: filterSections
+    });
   }
+  console.log(openSections);
+  return openSections;
 }
