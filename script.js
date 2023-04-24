bac8bb16345f7e684a743c47526a3683a5c7a3cb
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-04-23 22:07:57 -0400

    intitial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// An asynchronous data request to your API
+async function getData() {
+  const url = "https://api.umd.io/v0/courses/list";
+  const data = await fetch(url);
+  const json_data = await data.json();
+}
+
+// A processing request that uses array methods (.map, .filter, .find, .reduce) to change
+// your data into the shape your chart, map, or other component needs for display.
+async function processing_request() {
+  const courses = await getData();
+
+  const openCourses = [];
+
+  for (const course of courses) {
+    
+  }
+}
