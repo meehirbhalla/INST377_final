50c84367af88bdf986a0284b97e6f54abef506c8
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-04-30 23:49:38 -0400

    edits

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+async function getSectionIDs() {
+  const url = "https://api.umd.io/v0/courses/INST377/sections";
+  const data = await fetch(url);
+  const json_data = await data.json();
+  const sectionIDs = json_data.map((section) => section.section_id);
+  return sectionIDs;
+}
+
+getSectionIDs().then((ids) => console.log(ids));
 // An asynchronous data request to your API
 async function getData() {
   const url = "https://api.umd.io/v0/courses/INST377/sections";
@@ -10,24 +19,32 @@ async function getData() {
 // your data into the shape your chart, map, or other component needs for display.
 async function processing_request(selectedSections) {
   const courses = await getData();
+  const openSections = courses.filter((item) => item.open_seats > 0);
 
   // only display selected sections when using filter checkboxes
-  const filteredSections = openSections.filter((item) => selectedSections.includes(item.section_id));
+  const filteredSections = selectedSections.length
+    ? openSections.filter((item) => selectedSections.includes(item.section_id))
+    : openSections;
 
-  const openSections = courses.filter((item) => item.open_seats > 0);
-  const sections = openSections.map((item) => item.section_id);
-  const openSeats = openSections.map((item) => item.open_seats);
+  const sections = filteredSections.map((item) => item.section_id);
+
+  const openSeats = filteredSections.map((item) => item.open_seats);
 
   const chartTarget = document.querySelector("#myChart");
 
+  // checks if window.myChart is defined and if destroy is a function
+  if (window.myChart && typeof window.myChart.destroy === "function") {
+    window.myChart.destroy();
+  }
+
   // Data visualization using chart.js
-  const myChart = new Chart(chartTarget, {
+  window.myChart = new Chart(chartTarget, {
     type: "bar",
     data: {
       labels: sections,
       datasets: [
         {
-          label: "INST377 Open Seats",
+          label: "Open Seats",
           backgroundColor: "rgb(120, 120, 120)",
           borderColor: "rgb(0, 0, 0)",
           data: openSeats,
@@ -37,6 +54,18 @@ async function processing_request(selectedSections) {
     options: {},
   });
 }
+
+const filter = document.querySelectorAll('input[type="checkbox"]');
+
+filter.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    const selectedSections = Array.from(filter)
+      .filter((checkbox) => checkbox.checked)
+      .map((checkbox) => checkbox.value);
+
+    processing_request(selectedSections);
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   processing_request();
