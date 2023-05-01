3a0406cb1ce62d56a1d6130232c779adde9e983f
Meehir <98355618+meehirbhalla@users.noreply.github.com>
2023-04-30 20:17:28 -0400

    revert changes - new idea

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 // An asynchronous data request to your API
 async function getData() {
-  // target courses
-  const courses = ["INST377", "INST362", "INST354"]
-  courseData = {}
-  for (const course of courses) {
-    const url = "https://api.umd.io/v0/courses/${course}/sections";
-    const data = await fetch(url);
-    const json_data = await data.json();
-    // append json data for course into courseData object as a key value pair
-    courseData[course] = json_data;
-  }
-  return courseData;
+  const url = "https://api.umd.io/v0/courses/INST377/sections";
+  const data = await fetch(url);
+  const json_data = await data.json();
+  return json_data;
 }
 
 // A processing request that uses array methods (.map, .filter, .find, .reduce) to change
@@ -31,7 +24,7 @@ async function processing_request() {
       labels: sections,
       datasets: [
         {
-          label: "Open Seats",
+          label: "INST377 Open Seats",
           backgroundColor: "rgb(120, 120, 120)",
           borderColor: "rgb(0, 0, 0)",
           data: openSeats,
